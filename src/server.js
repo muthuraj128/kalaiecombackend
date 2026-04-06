d2ea50bc6dbf721e107cef2729163cb287db1879
MUTHURAJ <r.muthuraj128@gmail.com>
2026-04-07 00:11:39 +0530

    Delete src directory

--- src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app");
-const config = require("./config");
-
-app.listen(config.port, () => {
-  console.log(`Admin backend running on http://localhost:${config.port}`);
-});
